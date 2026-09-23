00942fd999e163f0341f6b3934ceb26b1d9b30f4
mode23 <nakopoulosth@gmail.com>
2026-09-23 21:02:20 +0300

    'init'

---
+++ part3/index.js
@@ -0,0 +1,75 @@
+//import node built in web server module
+// const http = require("http");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+// no express way
+// const app = http.createServer((request, response) => {
+//   response.writeHead(200, { "Content-type": "application/json" });
+//   response.end(JSON.stringify(notes));
+// });
+const generateId = () => {
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
+};
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1>");
+});
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    response.json(note);
+  } else {
+    response.statusMessage = "there is no note with this id";
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+app.post("/api/notes", (req, res) => {
+  if (!body.content) {
+    return res.status(400).json({ error: "content missing" });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    id: generateId(),
+  };
+  notes = notes.concat(note);
+  res.json(note);
+});
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`);
